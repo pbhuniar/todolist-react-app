166fcd5aa8af0b8703498c9fd0d06f42b9e71512
pranabr <pranabr@gmail.com>
2022-07-24 19:35:29 +0530

    push

--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -27,7 +27,7 @@ const Todolist = ({ todo, deletefromTodo, edit, handleClose, handleShow, setedit
                 <p><i>{"("}update {value?.updatetime} time {")"}</i></p>
                 </div>
              
-              <i style={{margin:"5px",color:"red"}} onClick={() => deletefromTodo(key)} class="fa-solid fa-circle-minus"></i>
+              <i style={{margin:"5px",color:"#b30e0e"}} onClick={() => deletefromTodo(key)} class="fa-solid fa-circle-minus"></i>
               
               <i style={{margin:"5px",color:"#054001"}}  onClick={() => { handleShow(); seteditindex(key) }} class="fa-solid fa-pen-to-square"></i>
             </div>
